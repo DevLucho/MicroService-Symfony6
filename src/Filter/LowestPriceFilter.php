0d439dbbbf8cd7b693f922745a8e59d6fe98f681
Luis Huertas <luisdavidhuertas@hotmail.com>
2022-09-03 22:03:33 -0500

    Add: Logic Modified Price

--- src/Filter/LowestPriceFilter.php
+++ src/Filter/LowestPriceFilter.php
@@ -10,8 +10,14 @@ class LowestPriceFilter implements PromotionsFilterInterface
 
     public function apply(PromotionEnquiryInterface $enquiry, Promotion ...$promotion): PromotionEnquiryInterface
     {
+        $price = $enquiry->getProduct()->getPrice();
+        $quantity = $enquiry->getQuantity();
+        $lowestPrice = $quantity * $price;
+
+
+
         // 3. Return the modified Enquiry
-        $enquiry->setDiscountedPrice(50);
+        $enquiry->setDiscountedPrice(250);
         $enquiry->setPrice(100);
         $enquiry->setPromotionId(3);
         $enquiry->setPromotionName('Black Friday half price sale');
