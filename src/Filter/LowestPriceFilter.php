e5d814fce082e0b98043c614d2dbc54febd13613
Luis Huertas <luisdavidhuertas@hotmail.com>
2022-09-03 23:00:15 -0500

    Add: Factory Class

--- src/Filter/LowestPriceFilter.php
+++ src/Filter/LowestPriceFilter.php
@@ -4,24 +4,31 @@ namespace App\Filter;
 
 use App\DTO\PromotionEnquiryInterface;
 use App\Entity\Promotion;
+use App\Filter\Modifier\Factory\PriceModifierFactoryInterface;
 
 class LowestPriceFilter implements PromotionsFilterInterface
 {
+    public function __construct(private PriceModifierFactoryInterface $priceModifierFactory)
+    {
 
-    public function apply(PromotionEnquiryInterface $enquiry, Promotion ...$promotion): PromotionEnquiryInterface
+    }
+
+    public function apply(PromotionEnquiryInterface $enquiry, Promotion ...$promotions): PromotionEnquiryInterface
     {
         $price = $enquiry->getProduct()->getPrice();
         $quantity = $enquiry->getQuantity();
         $lowestPrice = $quantity * $price;
 
+        foreach ($promotions as $promotion) {
 
 
-        // 3. Return the modified Enquiry
-        $enquiry->setDiscountedPrice(250);
-        $enquiry->setPrice(100);
-        $enquiry->setPromotionId(3);
-        $enquiry->setPromotionName('Black Friday half price sale');
-
+            $priceModifier = $this->priceModifierFactory->create($promotion->getType());
+            // 3. Return the modified Enquiry
+            $enquiry->setDiscountedPrice(250);
+            $enquiry->setPrice(100);
+            $enquiry->setPromotionId(3);
+            $enquiry->setPromotionName('Black Friday half price sale');
+        }
         return $enquiry;
     }
 }
